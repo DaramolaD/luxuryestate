f6e599684640a75e967473db1cd206717561125b
Daniel <projects@gtextsoft.com>
2025-11-21 01:03:33 +0100

    Add web app manifest and icons; remove unused SVG files

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,21 @@ export const metadata: Metadata = {
   authors: [{ name: "LuxuryEstate" }],
   creator: "LuxuryEstate",
   publisher: "LuxuryEstate",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "LuxuryEstate",
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
